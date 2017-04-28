d2938074447514170e9942a4d2178cf725ee881c
Lyrica <youmu-konpaku@outlook.com>
2017-04-28 14:12:19 +0800

    Update filterfile.py

--- filterfile.py
+++ filterfile.py
@@ -4,12 +4,12 @@
 import os, os.path, shutil
 from itimer import itimer
 
-#os.system(r'net use z: \\ip.ip.ip.ip\folder')
+#os.system(r'net use z: \\ip\folder')
 #复制局域网文件需先映射
 #原文件夹
-SRC = r'Z:\\'
+SRC = os.path.normpath('Z:\\')
 #目标文件夹
-DEST = r'D:\\'
+DEST = os.path.normpath('D:\\codes\\msc1')
 #记录日志的文件夹
 LOG = 'log.txt'
 #文件列表
@@ -18,7 +18,7 @@ all_files = []
 all_dirs = []
 LINESEP = os.linesep
 t = itimer()
-f = open(log, 'a')
+f = open(LOG, 'a')
 
 #遍历原文件夹并存储文件和文件夹信息
 def forfiles(path, files):
@@ -33,7 +33,7 @@ def forfiles(path, files):
 t.setStart()
 forfiles(SRC, os.listdir(SRC))
 #记录所用时间
-f.write('get all folders and files from %s uses %.4f s%s' % (SRC, t.getSec(), LINESEP)
+f.write('get all folders and files from %s uses %.4f s%s' % (SRC, t.getSec(), LINESEP))
 f.flush()
 #复制到目标文件夹
 def copyfiles(dirlist, filelist, dest):
@@ -42,16 +42,28 @@ def copyfiles(dirlist, filelist, dest):
        salt = 0
     #先创建文件夹
    for dir in dirlist:
-       os.mkdir(os.path.join(dest, dir[len(SRC) + salt:]))
-   for file in filelist:
-       destfile = os.path.join(dest, file[len(SRC) + salt:])
-       shutil.copy(file, destfile)
-       print('done: %s' % destfile)
+       destdir = os.path.join(dest, dir[len(SRC) + salt:])
+       if not os.path.exists(destdir):
+            os.mkdir(destdir)
+   for i in range(len(filelist)):
+        destfile = os.path.join(dest, filelist[i][len(SRC) + salt:])
+        if os.path.exists(destfile):
+            os.chmod(destfile, stat.S_IWRITE)
+            os.remove(destfile)
+        try:
+            shutil.copy(filelist[i], destfile)
+        except IOError as e:
+            f.write('[%d/%d]error: %s %s ' % (i, files_count, destfile, LINESEP))
+            f.write('I/O error: %s %s' % (e, LINESEP))
+            f.flush()
+            print('[%d/%d]error: %s' % (i, files_count, destfile))
+            pass
+        print('[%d/%d]done: %s' % (i, files_count, destfile))
 
 t.setStart()
 #筛选文件,筛选时对all_files和all_folder进行操作,这两都是一维列表
 filtered_dirs = [x for x in all_dirs if '.svn' not in x]
 filtered_files = [x for x in all_files if '.svn' not in x]
 copyfiles(filtered_dirs, filtered_files, DEST)
-f.write('copy all folders and files from %s to %s uses %.4f s%s' % (SRC, DEST, t.getSec(), LINESEP)
+f.write('copy all folders and files from %s to %s uses %.4f s%s' % (SRC, DEST, t.getSec(), LINESEP))
 f.close()
